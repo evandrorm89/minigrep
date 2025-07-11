50c1a74152f03c5e2acaa6296b16d100ae0cc21c
Evandro <evandrorm89@gmail.com>
2025-07-11 18:30:48 -0300

    Starting with minigrep project

---
+++ src/lib.rs
@@ -0,0 +1,75 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn build_config() {
+        let args = vec![
+            String::from("minigrep"),
+            String::from("query"),
+            String::from("file.txt"),
+        ];
+        let config = Config::build(&args).unwrap();
+        assert_eq!(config.query, "query");
+        assert_eq!(config.file_path, "file.txt");
+    }
+
+    #[test]
+    fn build_config_not_enough_args() {
+        let args = vec![String::from("minigrep")];
+        let result = Config::build(&args);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
