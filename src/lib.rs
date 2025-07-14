681ed887b770ef715155171e14c9e0dfe435783d
Evandro <evandrorm89@gmail.com>
2025-07-13 21:36:54 -0300

    improving code by using iterators

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -43,26 +49,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line.trim());
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line.trim());
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
@@ -75,16 +73,17 @@ mod tests {
             String::from("minigrep"),
             String::from("query"),
             String::from("file.txt"),
-        ];
-        let config = Config::build(&args).unwrap();
+        ]
+        .into_iter();
+        let config = Config::build(args).unwrap();
         assert_eq!(config.query, "query");
         assert_eq!(config.file_path, "file.txt");
     }
 
     #[test]
     fn build_config_not_enough_args() {
-        let args = vec![String::from("minigrep")];
-        let result = Config::build(&args);
+        let args = vec![String::from("minigrep")].into_iter();
+        let result = Config::build(args);
         assert!(result.is_err());
     }
 
@@ -92,10 +91,10 @@ mod tests {
     fn case_sensitve() {
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.
-        Duct tape";
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
@@ -104,10 +103,10 @@ mod tests {
     fn case_insensitive() {
         let query = "rUsT";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.
-        Trust me.";
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
 
         assert_eq!(
             vec!["Rust:", "Trust me."],
